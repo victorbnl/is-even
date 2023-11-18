7f5a5c3d7515c066a285e89a7757cf0aca1c246c
Victor Bonnelle <victor.bonnelle@protonmail.com>
2023-11-18 15:06:18 +0100

    Improve code

--- is_even/__init__.py
+++ is_even/__init__.py
@@ -1,4 +1,7 @@
 __version__ = "1.0.5"
 
-def is_even(n):
-    return int(n) % 2 == 0
+
+def is_even(number: int) -> bool:
+    """Return whether `number` is even."""
+
+    return number % 2 == 0
--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import unittest
 from is_even import is_even
 
+
 class Test(unittest.TestCase):
     def test(self):
         self.assertTrue(is_even(0))
         self.assertFalse(is_even(1))
         self.assertTrue(is_even(2))
         self.assertFalse(is_even(3))
+
 
 if __name__ == "__main__":
     unittest.main()
